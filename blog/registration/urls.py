48f2a31f1d5a8b10d7fd5ae663e3e6d7ccca2989
Lepskie Zakhar <lepskiezakhar@gmail.com>
2023-03-30 00:13:16 +0300

    Добавлена возможность регистрации

--- blog/registration/urls.py
+++ blog/registration/urls.py
@@ -8,5 +8,6 @@ app_name = 'registration'
 
 urlpatterns = [
     path("login/", views.LoginUser.as_view(), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(), name='logout')
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
+    path("registration/", views.RegisterUser.as_view(), name='registrate'),
 ]
--- blog/registration/views.py
+++ blog/registration/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import AuthenticationForm
-from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
 
 # Create your views here.
@@ -12,3 +13,9 @@ class LoginUser(LoginView):
 
     def get_success_url(self) -> str:
         return reverse('main:blogger-detail', args=[str(self.request.user.id)])
+
+
+class RegisterUser(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/registrate.html'
+    success_url = reverse_lazy('registration:login')
