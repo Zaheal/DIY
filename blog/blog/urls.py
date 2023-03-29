ff03513461879e12efd8fce2c817a098680733bb
Lepskie Zakhar <lepskiezakhar@gmail.com>
2023-03-29 11:02:50 +0300

    Some

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include('main.urls')),
+    path("registration/", include('registration.urls')),
 ]
